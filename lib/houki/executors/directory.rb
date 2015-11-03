c9cde4b43aaf5e69ce38a8c9d967f350f9b07d33
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-11-04 08:24:43 +0900

    display directories that remove

--- lib/houki/executors/directory.rb
+++ lib/houki/executors/directory.rb
@@ -1,13 +1,16 @@
 require 'fileutils'
+require 'terminal-table'
 
 module Houki::Executors
   class Directory
     EXCLUDED_DIRS = ['.', '..']
 
-    def self.run(paths)
-      paths.each do |path|
-        root_dirs = Dir.entries(path).reject { |f| EXCLUDED_DIRS.include?(f) }
-        FileUtils.rm_r(root_dirs.collect { |f| File.join(path, f) })
+    def self.run(directories)
+      table = Terminal::Table.new(title: 'Directories', rows: directories.combination(1).to_a)
+      $stdout.puts table
+      directories.each do |directory|
+        root_dirs = Dir.entries(directory).reject { |f| EXCLUDED_DIRS.include?(f) }
+        FileUtils.rm_r(root_dirs.collect { |f| File.join(directory, f) })
       end
     rescue Errno::ENOENT
     end
